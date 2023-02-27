b77d8cf57eb23b397af64a647174b24094625129
500153744 <500153744@fvtc.net>
2023-02-27 16:35:41 -0600

    added librarys

---
+++ README.txt
@@ -0,0 +1,38 @@
+### Infomation sources:<br>
+
+**RGB LEDs** <br>
+https://hackaday.com/2019/03/26/can-you-live-without-the-ws2812/ <br>
+https://cdn-shop.adafruit.com/datasheets/WS2811.pdf <br>
+https://hackaday.com/tag/ws2811/ <br>
+
+**LED Fonts** <br>
+https://damieng.com/blog/2011/02/20/typography-in-8-bits-system-fonts/ <br>
+https://sschueller.github.io/posts/vbz-fahrgastinformation/#the-font <br>
+https://www.dafont.com/8bit-wonder.font <br>
+
+**Display Images or Video** <br>
+https://stackoverflow.com/questions/26032660/difference-between-image-formats-rgb888-and-argb8888#26032691 <br>
+http://www.barth-dev.de/about-rgb565-and-how-to-convert-into-it/ <br>
+http://www.barth-dev.de/online/rgb565-color-picker/ <br>
+
+**Programming Code Samples** <br>
+https://github.com/bertrik/lichtkrant/blob/master/ledbanner2udp.c <br>
+RGB565 = (((RGB888&0xf80000)>>8) + ((RGB888&0xfc00)>>5) + ((RGB888&0xf8)>>3)); <br>
+https://github.com/mrfaptastic/Glediator3 <br>
+
+**LED Matrix controlling software** <br>
+Jinx - LED Matrix Control http://www.live-leds.de/downloads/ <br>
+Glediator - LED Matrix Control https://github.com/mrfaptastic/Glediator3 <br>
+FastLed Library Example https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples#one-array-many-strips <br>
+
+**Protocol for LED comunications** <br>
+Art-Net, sACN (E1.31), tpm2.net, tpm2, Glediator protocol and MiniDMX as well as real DMX <br>
+TPM2 Protocol Implementation https://gist.github.com/jblang/89e24e2655be6c463c56 <br>
+TPM2 protocol for LED comunications https://github.com/rstephan/TPM2 <br>
+
+
+
+
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -19,9 +19,23 @@
 #include <Arduino.h>
 
 #include <FastLED.h>
-#define RGB_NUM 50							// RGB LEDs quantity
-#define RGB_PIN 4							// RGB data pin
-CRGB leds[RGB_NUM];							// Declare RGB structure
+
+#define NUM_STRIPS 22
+#define NUM_LEDS_PER_STRIP 42
+#define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
+CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
+
+#include <PCF8575.h>
+TwoWire I2Cone = TwoWire(0);				// Instantiate Wire for use at 400kHz
+TwoWire I2Ctwo = TwoWire(0);				// Instantiate Wire for use at 400kHz
+
+// PCF8575 address map 0x20-0x27 use A0, A1, A2 to change address
+PCF8575 expander1(&I2Cone, 0x20);				// Set I2C address for I2C one
+PCF8575 expander2(&I2Ctwo, 0x21);				// Set I2C address for I2C two
+
+// #define RGB_NUM 50							// RGB LEDs quantity
+// #define RGB_PIN 4							// RGB data pin
+// CRGB leds[RGB_NUM];							// Declare RGB structure
 
 unsigned long timePrev = millis();			// Non-Blocking LED heartbeat
 unsigned long time10 = millis();			// Non-Blocking timing
@@ -37,7 +51,38 @@ void setup()
 	Serial.begin(115200);					// Serial monitor baud rate 115200				
 	pinMode(LED_BUILTIN, OUTPUT);			// Set pin to function as an output
 
-	FastLED.addLeds<WS2811, RGB_PIN, RGB>(leds, RGB_NUM);
+	I2Cone.begin(0x20, 21, 22, 400000);		// Wire begin (address, pins, speed)
+	I2Ctwo.begin(0x21, 21, 22, 400000);		// Wire begin (address, pins, speed)
+
+	for(int i=0; i<16; i++)					// Set pinMode to OUTPUT
+	{
+		expander1.pinMode(i, OUTPUT);
+		expander2.pinMode(i, OUTPUT);
+	}
+	expander1.begin();
+	expander2.begin();
+
+	// expander1 pin P0, starting at index 0 in led array
+	FastLED.addLeds<WS2811, P0, RGB>(leds, 0, NUM_LEDS_PER_STRIP);
+	// expander1 pin P1, starting at index 42 in led array	
+	FastLED.addLeds<WS2811, P1, RGB>(leds, NUM_LEDS_PER_STRIP*2, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P2, RGB>(leds, NUM_LEDS_PER_STRIP*3, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P3, RGB>(leds, NUM_LEDS_PER_STRIP*4, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P4, RGB>(leds, NUM_LEDS_PER_STRIP*5, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P5, RGB>(leds, NUM_LEDS_PER_STRIP*6, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P6, RGB>(leds, NUM_LEDS_PER_STRIP*7, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P7, RGB>(leds, NUM_LEDS_PER_STRIP*8, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P8, RGB>(leds, NUM_LEDS_PER_STRIP*9, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P9, RGB>(leds, NUM_LEDS_PER_STRIP*10, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P10, RGB>(leds, NUM_LEDS_PER_STRIP*11, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P11, RGB>(leds, NUM_LEDS_PER_STRIP*12, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P12, RGB>(leds, NUM_LEDS_PER_STRIP*13, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P13, RGB>(leds, NUM_LEDS_PER_STRIP*14, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P14, RGB>(leds, NUM_LEDS_PER_STRIP*15, NUM_LEDS_PER_STRIP);
+	FastLED.addLeds<WS2811, P15, RGB>(leds, NUM_LEDS_PER_STRIP*16, NUM_LEDS_PER_STRIP);	
+
+
+
 }
 
 
@@ -51,13 +96,13 @@ void loop()
 		ledState = !ledState;				// Toggle LED on/off
 		digitalWrite(LED_BUILTIN, ledState);	
 
-		// Turn on all 50 leds
-		for (uint8_t i=0; i<RGB_NUM; i++)
-		{
-			leds[i] = CRGB::Yellow;
-		 	FastLED.setBrightness(28);		
-	 		FastLED.show();			
-		}
+		// // Turn on all 50 leds
+		// for (uint8_t i=0; i<RGB_NUM; i++)
+		// {
+		// 	leds[i] = CRGB::Yellow;
+		//  	FastLED.setBrightness(28);		
+	 	// 	FastLED.show();			
+		// }
 	}
 
 	// This turns on multiple colors
