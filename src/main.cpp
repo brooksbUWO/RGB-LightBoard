5aadafad7ae6e3002e971216efce8a7f2ba3ac6b
500153744 <500153744@fvtc.net>
2023-02-28 14:21:59 -0600

    FastLed setup for entire matrix

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,13 @@ unsigned long timeDelay = millis();			// Non-Blocking timing delay
 uint8_t ledState = LOW;						// State of LED
 uint16_t counter = 0;						// Counter
 
+// https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples#one-array-many-strips
+#include <FastLED.h>
+#define NUM_STRIPS 22
+#define NUM_LEDS_PER_STRIP 42
+#define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
+CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
+
 // https://www.thecoderscorner.com/products/arduino-downloads/io-abstraction/
 #include <IoAbstraction.h>
 #include <IoAbstractionWire.h>
@@ -32,13 +39,6 @@ uint16_t counter = 0;						// Counter
 MultiIoAbstraction multiIo(100);			// Allocate 100 pins to arduino pins
 PCF8574IoAbstraction expander1(0x20, IO_PIN_NOT_DEFINED);
 PCF8574IoAbstraction expander2(0x21, IO_PIN_NOT_DEFINED);
-
-// https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples#one-array-many-strips
-#include <FastLED.h>
-#define NUM_STRIPS 22
-#define NUM_LEDS_PER_STRIP 42
-#define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
-CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
 
 
 // Begin Code
@@ -58,9 +58,9 @@ void setup()
 	}
 
 	// Pin abstraction needs line 358 in clockless_rmt_esp32.h commented out
-	// expander1 pin P0 abstracted to pin 100, starting at index 0 in led array
+	// Expander1 pin P0 abstracted to pin 100, starting at index 0 in led array
 	FastLED.addLeds<WS2811, 100, RGB>(leds, 0, NUM_LEDS_PER_STRIP);
-	// expander1 pin P1 abstracted to pin 101, starting at index 42 in led array
+	// Expander1 pin P1 abstracted to pin 101, starting at index 42 in led array
 	FastLED.addLeds<WS2811, 101, RGB>(leds, NUM_LEDS_PER_STRIP*1, NUM_LEDS_PER_STRIP);
 	FastLED.addLeds<WS2811, 102, RGB>(leds, NUM_LEDS_PER_STRIP*2, NUM_LEDS_PER_STRIP);
 	FastLED.addLeds<WS2811, 103, RGB>(leds, NUM_LEDS_PER_STRIP*3, NUM_LEDS_PER_STRIP);
@@ -77,9 +77,9 @@ void setup()
 	FastLED.addLeds<WS2811, 114, RGB>(leds, NUM_LEDS_PER_STRIP*14, NUM_LEDS_PER_STRIP);
 	FastLED.addLeds<WS2811, 115, RGB>(leds, NUM_LEDS_PER_STRIP*15, NUM_LEDS_PER_STRIP);	
 
-	// expander2 pin P0 abstracted to pin 116, starting at index 714 in led array
+	// Expander2 pin P0 abstracted to pin 116, starting at index 714 in led array
 	FastLED.addLeds<WS2811, 116, RGB>(leds, NUM_LEDS_PER_STRIP*16, NUM_LEDS_PER_STRIP);	
-	// expander2 pin P1 abstracted to pin 117, starting at index 756 in led array
+	// Expander2 pin P1 abstracted to pin 117, starting at index 756 in led array
 	FastLED.addLeds<WS2811, 117, RGB>(leds, NUM_LEDS_PER_STRIP*17, NUM_LEDS_PER_STRIP);	
 	FastLED.addLeds<WS2811, 118, RGB>(leds, NUM_LEDS_PER_STRIP*18, NUM_LEDS_PER_STRIP);	
 	FastLED.addLeds<WS2811, 119, RGB>(leds, NUM_LEDS_PER_STRIP*19, NUM_LEDS_PER_STRIP);	
