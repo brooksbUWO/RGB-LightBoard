aa2ec6c2b3603f386e2e817f895de305c3b7c227
500153744 <500153744@fvtc.net>
2024-02-13 13:18:44 -0600

    added state machine

--- src/main.cpp
+++ src/main.cpp
@@ -11,48 +11,47 @@
 // When			    Who	    Description of change
 // -----------	-----------	-----------------------
 // 22-FEB-2023	brooks		  Start of program
-//
+// 13-FEB-2024	brooks		  Added Finite State Machine
 //
 // ****************************************************************************
-
 
 // Include Files
 // ****************************************************************************
 #include <Arduino.h>
 #include <main.h>
-//#define DEBUG
+// #define DEBUG
 
-unsigned long timePrev = millis();			// Non-Blocking LED heartbeat
-uint8_t ledState = LOW;						// State of LED
+unsigned long timePrev = millis(); // Non-Blocking LED heartbeat
+uint8_t ledState = LOW;			   // State of LED
 
-unsigned long time1 = millis();				// Non-Blocking timing delay
-unsigned long timeDelay1 = 70000;			// Time in milliseconds
-bool flagRun1 = false;						// Flag for time period
+unsigned long time1 = millis();	  // Non-Blocking timing delay
+unsigned long timeDelay1 = 70000; // Time in milliseconds
+bool flagRun1 = false;			  // Flag for time period
 
-unsigned long time2 = millis();				// Non-Blocking timing delay
-unsigned long timeDelay2 = timeDelay1*2;	// Time in milliseconds
-bool flagRun2 = false;						// Flag for time period
+unsigned long time2 = millis();			   // Non-Blocking timing delay
+unsigned long timeDelay2 = timeDelay1 * 2; // Time in milliseconds
+bool flagRun2 = false;					   // Flag for time period
 
-unsigned long time3 = millis();				// Non-Blocking timing delay
-unsigned long timeDelay3 = timeDelay2*2;	// Time in milliseconds
-bool flagRun3 = false;						// Flag for time period
+unsigned long time3 = millis();			   // Non-Blocking timing delay
+unsigned long timeDelay3 = timeDelay2 * 2; // Time in milliseconds
+bool flagRun3 = false;					   // Flag for time period
 
 // LCD hd44780
 // https://reference.arduino.cc/reference/en/libraries/hd44780/
 #include <Wire.h>
-#include <hd44780.h>						// Main hd44780 header
-#include <hd44780ioClass/hd44780_I2Cexp.h>	// I2C expander I/O class header
-hd44780_I2Cexp lcd;							// LCD object config expander chip
-const int LCD_COLS = 20;					// LCD columns
-const int LCD_ROWS = 4;						// LCD rows
+#include <hd44780.h>					   // Main hd44780 header
+#include <hd44780ioClass/hd44780_I2Cexp.h> // I2C expander I/O class header
+hd44780_I2Cexp lcd;						   // LCD object config expander chip
+const int LCD_COLS = 20;				   // LCD columns
+const int LCD_ROWS = 4;					   // LCD rows
 
 // FastLed library
 // https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples#one-array-many-strips
 #include <FastLED.h>
-#define NUM_LEDS 	924
-#define DATA_PIN 	4						// RGB led data pin
-#define COLOR_ORDER RGB						// RGB led color order
-#define CHIPSET 	WS2811					// RGB led type
+#define NUM_LEDS 924
+#define DATA_PIN 4		// RGB led data pin
+#define COLOR_ORDER RGB // RGB led color order
+#define CHIPSET WS2811	// RGB led type
 CRGB leds[NUM_LEDS];
 
 // RGB Matrix, Text & Sprite libraries
@@ -61,42 +60,42 @@ CRGB leds[NUM_LEDS];
 #include <LEDMatrix.h>
 #include <LEDText.h>
 #include <FontCourierNew7x11.h>
-#define MATRIX_WIDTH   42
-#define MATRIX_HEIGHT  -22
-#define MATRIX_TYPE    HORIZONTAL_ZIGZAG_MATRIX
-// Instantiate an instance of the cLEDMatrix class
-cLEDMatrix<MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> matrix;
-cLEDText display;						// Declare cLEDText class variable
-
+#define MATRIX_WIDTH 42
+#define MATRIX_HEIGHT -22
+#define MATRIX_TYPE HORIZONTAL_ZIGZAG_MATRIX
+cLEDMatrix<MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> matrix; // Instantiate cLEDMatrix class
+cLEDText display;											 // Declare cLEDText class variable
 uint8_t hue = 0;
 int16_t counter = 0;
 
 // Various things to display on the RGB matrix
-const unsigned char TxtDemo[] = { 
-	EFFECT_SCROLL_LEFT "            LEFT SCROLL "
-    EFFECT_SCROLL_RIGHT "            LLORCS THGIR"
-	EFFECT_SCROLL_DOWN "            SCROLL DOWN             SCROLL DOWN            " EFFECT_FRAME_RATE "\x04" " SCROLL DOWN            " EFFECT_FRAME_RATE "\x00" " "
-	EFFECT_SCROLL_UP "             SCROLL UP               SCROLL UP             " EFFECT_FRAME_RATE "\x04" "  SCROLL UP             " EFFECT_FRAME_RATE "\x00" " "
-	EFFECT_CHAR_UP EFFECT_SCROLL_LEFT "            UP"
-	EFFECT_CHAR_RIGHT "  RIGHT"
-	EFFECT_CHAR_DOWN "  DOWN"
-	EFFECT_CHAR_LEFT "  LEFT"
-	EFFECT_HSV_CV "\x00\xff\xff\x40\xff\xff" EFFECT_CHAR_UP "           HSV_CV 00-40"
-	EFFECT_HSV_CH "\x00\xff\xff\x40\xff\xff" "    HSV_CH 00-40"
-	EFFECT_HSV_AV "\x00\xff\xff\x40\xff\xff" "    HSV_AV 00-40"
-	EFFECT_HSV_AH "\x00\xff\xff\xff\xff\xff" "    HSV_AH 00-FF"
-	"           " EFFECT_HSV "\x00\xff\xff" "R" EFFECT_HSV "\x20\xff\xff" "A" EFFECT_HSV "\x40\xff\xff" "I" EFFECT_HSV "\x60\xff\xff" "N" EFFECT_HSV "\xe0\xff\xff" "B" EFFECT_HSV "\xc0\xff\xff" "O"
-	EFFECT_HSV "\xa0\xff\xff" "W" EFFECT_HSV "\x80\xff\xff" "S " EFFECT_DELAY_FRAMES "\x00\x96" EFFECT_RGB "\xff\xff\xff" 
-};
+const unsigned char TxtDemo[] = {
+	EFFECT_SCROLL_LEFT "            LEFT SCROLL " EFFECT_SCROLL_RIGHT "            LLORCS THGIR" EFFECT_SCROLL_DOWN "            SCROLL DOWN             SCROLL DOWN            " EFFECT_FRAME_RATE "\x04"
+					   " SCROLL DOWN            " EFFECT_FRAME_RATE "\x00"
+					   " " EFFECT_SCROLL_UP "             SCROLL UP               SCROLL UP             " EFFECT_FRAME_RATE "\x04"
+					   "  SCROLL UP             " EFFECT_FRAME_RATE "\x00"
+					   " " EFFECT_CHAR_UP EFFECT_SCROLL_LEFT "            UP" EFFECT_CHAR_RIGHT "  RIGHT" EFFECT_CHAR_DOWN "  DOWN" EFFECT_CHAR_LEFT "  LEFT" EFFECT_HSV_CV "\x00\xff\xff\x40\xff\xff" EFFECT_CHAR_UP "           HSV_CV 00-40" EFFECT_HSV_CH "\x00\xff\xff\x40\xff\xff"
+					   "    HSV_CH 00-40" EFFECT_HSV_AV "\x00\xff\xff\x40\xff\xff"
+					   "    HSV_AV 00-40" EFFECT_HSV_AH "\x00\xff\xff\xff\xff\xff"
+					   "    HSV_AH 00-FF"
+					   "           " EFFECT_HSV "\x00\xff\xff"
+					   "R" EFFECT_HSV "\x20\xff\xff"
+					   "A" EFFECT_HSV "\x40\xff\xff"
+					   "I" EFFECT_HSV "\x60\xff\xff"
+					   "N" EFFECT_HSV "\xe0\xff\xff"
+					   "B" EFFECT_HSV "\xc0\xff\xff"
+					   "O" EFFECT_HSV "\xa0\xff\xff"
+					   "W" EFFECT_HSV "\x80\xff\xff"
+					   "S " EFFECT_DELAY_FRAMES "\x00\x96" EFFECT_RGB "\xff\xff\xff"};
 
-
+// Display text message with various effects
 const unsigned char displayMe[] = {
-	EFFECT_FRAME_RATE "\x04" EFFECT_SCROLL_UP "UWO"
-	EFFECT_FRAME_RATE "\x04" EFFECT_SCROLL_DOWN "UWO"
-	EFFECT_FRAME_RATE "\x04" EFFECT_SCROLL_LEFT "UWO"
-	EFFECT_DELAY_FRAMES "\x00\x96"
-};
+	EFFECT_FRAME_RATE "\x04" EFFECT_SCROLL_UP "UWO" EFFECT_FRAME_RATE "\x04" EFFECT_SCROLL_DOWN "UWO" EFFECT_FRAME_RATE "\x04" EFFECT_SCROLL_LEFT "UWO" EFFECT_DELAY_FRAMES "\x00\x96"};
 
+// Finite State Machine (FSM)
+// https://github.com/LennartHennigs/SimpleFSM
+#include <SimpleFSM.h> // Include the library
+SimpleFSM fsm;		   // Create an instance of the FSM
 
 // Functions
 // ****************************************************************************
@@ -104,56 +103,106 @@ void initLCD()
 {
 	int status;
 	status = lcd.begin(LCD_COLS, LCD_ROWS);
-	if(status) // non zero status means it was unsuccesful
-	{
-		// hd44780 has a fatalError() routine that blinks an led if possible
-		// begin() failed so blink error code using the onboard LED if possible
-		hd44780::fatalError(status); // does not return
-	}
 }
 
-void reboot()
+void state1()
 {
-	unsigned long rebootMillis = millis();		// Count down for rebooting
-	unsigned long rebootCountMillis = millis();	// 1s counter for GUI
-	uint16_t rebootTime = 10000;				// Reboot time in msec
-	uint16_t rebootCountDown = rebootTime/1000;	// Reboot count down in sec
-
-	Serial.printf("Restarting in %d seconds \r\n", rebootTime/1000);
-	for (;;)
-	{
-		if ( (millis() - rebootMillis > rebootTime) )
-		{
-			Serial.printf("....Restarting \r\n");
-			ESP.restart();
-		}
-		else if ( (millis() - rebootCountMillis >= 1000) )
-		{
-			rebootCountMillis = millis();
-			rebootCountDown = rebootCountDown - 1;
-			for(int i=rebootCountDown; i>0; i--)
-			{
-				Serial.print(".");
-			}
-			Serial.printf("%d \r\n", rebootCountDown);
-		}
-	}
+	display.SetFont(FontCourierNew7x11Data);
+	display.Init(&matrix, matrix.Width(), display.FontHeight() + 1, 0, 0);
+	display.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
+	display.SetTextColrOptions(COLR_RGB | COLR_SINGLE, 0xff, 0xff, 0x00); // Yellow
 }
 
+void state2()
+{
+	display.SetFont(FontCourierNew7x11Data);
+	display.Init(&matrix, matrix.Width(), display.FontHeight() + 1, 0, 0);
+	display.SetText((unsigned char *)displayMe, sizeof(displayMe) - 1);
+	display.SetTextColrOptions(COLR_RGB | COLR_SINGLE, 0xff, 0xff, 0x00); // Yellow
+}
+
+// Draw shapes and stuff on the RGB matrix
+void state3()
+{
+	int16_t sx, sy, x, y;
+	uint8_t h;
+
+	FastLED.clear();
+
+	h = hue;
+	if (counter < 1125)
+	{
+		// ** Fill LED's with diagonal stripes
+		for (x = 0; x < (matrix.Width() + matrix.Height()); ++x)
+		{
+			matrix.DrawLine(x - matrix.Height(), matrix.Height() - 1, x, 0, CHSV(h, 255, 255));
+			h += 16;
+		}
+	}
+	else
+	{
+		// ** Fill LED's with horizontal stripes
+		for (y = 0; y < matrix.Height(); ++y)
+		{
+			matrix.DrawLine(0, y, matrix.Width() - 1, y, CHSV(h, 255, 255));
+			h += 16;
+		}
+	}
+	hue += 4;
+
+	if (counter < 125)
+		;
+	else if (counter < 375)
+		matrix.HorizontalMirror();
+	else if (counter < 625)
+		matrix.VerticalMirror();
+	else if (counter < 875)
+		matrix.QuadrantMirror();
+	else if (counter < 1125)
+		matrix.QuadrantRotateMirror();
+	else if (counter < 1250)
+		;
+	else if (counter < 1500)
+		matrix.TriangleTopMirror();
+	else if (counter < 1750)
+		matrix.TriangleBottomMirror();
+	else if (counter < 2000)
+		matrix.QuadrantTopTriangleMirror();
+	else if (counter < 2250)
+		matrix.QuadrantBottomTriangleMirror();
+
+	counter++;
+	if (counter >= 2250)
+		counter = 0;
+	FastLED.show();
+}
+
+State s[] = {
+	State("textDemo", state1),
+	State("displayMe", state2),
+	State("drawStuff", state3)};
+
+TimedTransition timedTransitions[] = {
+	TimedTransition(&s[0], &s[1], 60000),
+	TimedTransition(&s[1], &s[2], 60000),
+	TimedTransition(&s[2], &s[0], 60000),
+};
+
+int num_timed = sizeof(timedTransitions) / sizeof(TimedTransition);
 
 // Begin Code
 // ****************************************************************************
-void setup() 
+void setup()
 {
-	Serial.begin(115200);					// Serial monitor baud rate 115200
+	Serial.begin(115200); // Serial monitor baud rate 115200
 	Serial.println();
 
-	initLCD();								// Initialize LCD display
-	pinMode(LED_BUILTIN, OUTPUT);			// Set pin function as an OUTPUT
+	initLCD();					  // Initialize LCD display
+	pinMode(LED_BUILTIN, OUTPUT); // Set pin function as an OUTPUT
 
 	FastLED.addLeds<CHIPSET, DATA_PIN, COLOR_ORDER>(matrix[0], matrix.Size());
 	// Test matrix on start-up by displaying some colors
-  	FastLED.setBrightness(255);	
+	FastLED.setBrightness(255);
 	FastLED.clear(true);
 #ifdef DEBUG
 	FastLED.showColor(CRGB::Red);
@@ -164,109 +213,31 @@ void setup()
 	delay(1000);
 	FastLED.showColor(CRGB::White);
 	delay(1000);
-  	FastLED.clear(true);
+	FastLED.clear(true);
 #endif
 	FastLED.show();
 
-	display.SetFont(FontCourierNew7x11Data);
-	display.Init(&matrix, matrix.Width(), display.FontHeight() + 1, 0, 0);
-	display.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
-	//display.SetText((unsigned char *)displayMe, sizeof(displayMe) - 1);
-	display.SetTextColrOptions(COLR_RGB | COLR_SINGLE, 0xff, 0xff, 0x00);	// Yellow
-	
-	time1 = millis();
-	time2 = millis();
+	fsm.add(timedTransitions, num_timed);
+	fsm.setInitialState(&s[0]);
 }
-
 
 // Main program (example display text)
 // ****************************************************************************
-void loop() 
+void loop()
 {
+	fsm.run(); // run the FSM
 
-	if ( millis()-timePrev >= 1000 )		// Repeats every 1sec
+	if (millis() - timePrev >= 1000) // Repeats every 1sec
 	{
-		timePrev = millis();				// Reset time delay
-		ledState = !ledState;				// Toggle LED on/off
+		timePrev = millis();  // Reset time delay
+		ledState = !ledState; // Toggle LED on/off
 		digitalWrite(LED_BUILTIN, ledState);
 
-		lcd.setCursor(0,0);
+		lcd.setCursor(0, 0);
 		lcd.print("UWO Lightboard");
-		lcd.setCursor(0,1);
+		lcd.setCursor(0, 1);
 		lcd.print("TimePrev=");
 		lcd.print(String(timePrev));
 		lcd.print("ms");
 	}
-
-	if (display.UpdateText() == -1)
-		display.SetText((unsigned char *)TxtDemo, sizeof(TxtDemo) - 1);
-		//display.SetText((unsigned char *)displayMe, sizeof(displayMe) - 1);
-	else if ( (millis()-time1 >= timeDelay1) )
-	{
-		drawStuff();
-	}
-	else
-		FastLED.show();
-	
-	if ( millis()-time2 >= timeDelay2 )
-		reboot();
-
-}
-
-
-// Draw shapes and stuff
-// ****************************************************************************
-void drawStuff() 
-{
-  int16_t sx, sy, x, y;
-  uint8_t h;
-
-  FastLED.clear();
-  
-  h = hue;
-  if (counter < 1125)
-  {
-    // ** Fill LED's with diagonal stripes
-    for (x=0; x<(matrix.Width()+matrix.Height()); ++x)
-    {
-      matrix.DrawLine(x - matrix.Height(), matrix.Height() - 1, x, 0, CHSV(h, 255, 255));
-      h+=16;
-    }
-  }
-  else
-  {
-    // ** Fill LED's with horizontal stripes
-    for (y=0; y<matrix.Height(); ++y)
-    {
-      matrix.DrawLine(0, y, matrix.Width() - 1, y, CHSV(h, 255, 255));
-      h+=16;
-    }
-  }
-  hue+=4;
-
-  if (counter < 125)
-    ;
-  else if (counter < 375)
-    matrix.HorizontalMirror();
-  else if (counter < 625)
-    matrix.VerticalMirror();
-  else if (counter < 875)
-    matrix.QuadrantMirror();
-  else if (counter < 1125)
-    matrix.QuadrantRotateMirror();
-  else if (counter < 1250)
-    ;
-  else if (counter < 1500)
-    matrix.TriangleTopMirror();
-  else if (counter < 1750)
-    matrix.TriangleBottomMirror();
-  else if (counter < 2000)
-    matrix.QuadrantTopTriangleMirror();
-  else if (counter < 2250)
-    matrix.QuadrantBottomTriangleMirror();
-
-  counter++;
-  if (counter >= 2250)
-    counter = 0;
-  FastLED.show();
 }
